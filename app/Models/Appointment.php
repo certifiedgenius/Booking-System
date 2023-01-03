d4ec2ec353ea485cb5b580f5d8f7ce39cd6f0fd1
certifiedgenius <mugeeramansoor@live.com>
2023-01-03 14:53:14 +0100

    Created the respective models

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'customer_id', 'barber_id', 'date', 'start_time', 'duration', 'timestamps', 'notes',
+    ];
 }
