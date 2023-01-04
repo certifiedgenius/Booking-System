77471305ab6312a52d9077fbaade9517b1322706
certifiedgenius <mugeeramansoor@live.com>
2023-01-04 15:09:40 +0100

    functions to hash password & get appointments

--- app/Models/Barber.php
+++ app/Models/Barber.php
@@ -15,6 +15,38 @@ class Barber extends Model
         'first_name', 'last_name', 'email', 'password',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Hash the password when it is set.
+     *
+     * @param string $value
+     * @return void
+     */
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    /**
+     * Get the appointments for the barber.
+     */
     public function appointments()
     {
         return $this->hasMany(Appointment::class);
