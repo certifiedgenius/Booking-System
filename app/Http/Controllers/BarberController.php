9193e81488cab0bb5abbed57875f6ca06c230bd5
certifiedgenius <mugeeramansoor@live.com>
2023-01-13 11:55:45 +0100

    Cleaned up the BarberController fille

--- app/Http/Controllers/BarberController.php
+++ app/Http/Controllers/BarberController.php
@@ -10,34 +10,38 @@ use Illuminate\Support\Facades\Hash;
 
 class BarberController extends Controller
 {
-    //
+
+    
     /**
-     * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
+    // retrieve and display data from the application's database. 
     public function index()
     {
         $barbers = Barber::all();
         return view('barbers.index', compact('barbers'));
     }
 
+    
+    
+    
     /**
-     * Show the form for creating a new resource.
-     *
      * @return \Illuminate\Http\Response
      */
+    // display a form for creating a new resource. 
     public function create()
     {
         return view('barbers.create');
     }
 
+    
+    
+    
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request 
      * @return \Illuminate\Http\Response
      */
+    // validate, sanitize, and store the data entered by the user into the database. to handle requests to store a new resource.
     public function store(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -63,60 +67,67 @@ class BarberController extends Controller
         return redirect()->route('barbers.index')->with('success', 'Barber added successfully');
     }
 
+    
+    
+    
     /**
-     * Display the specified resource.
-     *
-     * @param  \App\Barber  $barber
+     * @param  \App\Barber
      * @return \Illuminate\Http\Response
      */
+    // retrieve and display the details of a specific resource, such as displaying the details of a specific customer. 
     public function show(Barber $barber)
     {
         return view('barbers.show', compact('barber'));
     }
 
+    
+    
+    
     /**
-     * Show the form for editing the specified resource.
-     *
      * @param  \App\Barber  $barber
      * @return \Illuminate\Http\Response
      */
+    // handle requests to display a form for editing a specific resource.
     public function edit(Barber $barber)
     {
         return view('barbers.edit', compact('barber'));
     }
 
     
+    
+    
+    // validate, sanitize, and update the data entered by the user into the database for a specific resource.
     public function update(Request $request, $id)
-{
-    $validator = Validator::make($request->all(), [
-        'first_name' => 'required|string|max:255',
-        'last_name' => 'required|string|max:255',
-        'email' => 'required|email|unique:barbers,email,' . $id,
-        'password' => 'nullable|string|min:6',
-    ]);
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|unique:barbers,email,' . $id,
+            'password' => 'nullable|string|min:6',
+        ]);
 
-    if ($validator->fails()) {
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'errors' => $validator->errors(),
+            ], 422);
+        }
+
+        $barber = Barber::findOrFail($id);
+        $barber->first_name = $request->input('first_name');
+        $barber->last_name = $request->input('last_name');
+        $barber->email = $request->input('email');
+
+        if ($request->has('password')) {
+            $barber->password = Hash::make($request->input('password'));
+        }
+
+        $barber->save();
+
         return response()->json([
-            'status' => 'error',
-            'errors' => $validator->errors(),
-        ], 422);
+            'status' => 'success',
+            'barber' => $barber,
+        ], 200);
     }
-
-    $barber = Barber::findOrFail($id);
-    $barber->first_name = $request->input('first_name');
-    $barber->last_name = $request->input('last_name');
-    $barber->email = $request->input('email');
-
-    if ($request->has('password')) {
-        $barber->password = Hash::make($request->input('password'));
-    }
-
-    $barber->save();
-
-    return response()->json([
-        'status' => 'success',
-        'barber' => $barber,
-    ], 200);
-}
     
 }
